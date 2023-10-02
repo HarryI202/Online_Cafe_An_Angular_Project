b8600ca3007935588f868146ab7561eb775be3ca
Amit <amitkumar@evincedev.com>
2023-10-02 17:42:55 +0530

    my changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './User/login/login.component';
 import { RegistationComponent } from './User/registation/registation.component';
+import { HomeComponent } from './Pages/home/home.component';
+import { BkftComponent } from './Pages/bkft/bkft.component';
+import { AppetizersComponent } from './Pages/appetizers/appetizers.component';
+import { DrinksComponent } from './Pages/drinks/drinks.component';
+import { MenuComponent } from './Pages/menu/menu.component';
 
 const routes: Routes = [
   {
@@ -9,7 +14,26 @@ const routes: Routes = [
   },
   {
     path:'rigister',component:RegistationComponent
-  }
+  },
+  {
+    path:'',component:HomeComponent
+  },
+  {
+    path:'home',component:HomeComponent
+  },
+  {
+    path:'apptizer',component:AppetizersComponent
+  },
+  {
+    path:'drinks',component:DrinksComponent
+  },
+  {
+    path:'menu',component:MenuComponent
+  },
+  {
+    path:'bkft',component:BkftComponent
+  },
+
 ];
 
 @NgModule({
