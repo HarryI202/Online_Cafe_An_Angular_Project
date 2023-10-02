6b6b5833e0db7e8f9e4d3258bfb81358044e8192
Amit <amitkumar@evincedev.com>
2023-10-02 17:43:10 +0530

    Revert "my changes"
    This reverts commit b8600ca3007935588f868146ab7561eb775be3ca.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './User/login/login.component';
 import { RegistationComponent } from './User/registation/registation.component';
-import { HomeComponent } from './Pages/home/home.component';
-import { BkftComponent } from './Pages/bkft/bkft.component';
-import { AppetizersComponent } from './Pages/appetizers/appetizers.component';
-import { DrinksComponent } from './Pages/drinks/drinks.component';
-import { MenuComponent } from './Pages/menu/menu.component';
 
 const routes: Routes = [
   {
@@ -14,26 +9,7 @@ const routes: Routes = [
   },
   {
     path:'rigister',component:RegistationComponent
-  },
-  {
-    path:'',component:HomeComponent
-  },
-  {
-    path:'home',component:HomeComponent
-  },
-  {
-    path:'apptizer',component:AppetizersComponent
-  },
-  {
-    path:'drinks',component:DrinksComponent
-  },
-  {
-    path:'menu',component:MenuComponent
-  },
-  {
-    path:'bkft',component:BkftComponent
-  },
-
+  }
 ];
 
 @NgModule({
